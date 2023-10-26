d7ca87447a735f87f5f97a842e374593824ab12a
Maxime Bignolet <m.bignolet@rhinos.fr>
2023-10-26 09:52:40 +0200

    add tailwind css config

---
+++ index.js
@@ -0,0 +1,12 @@
+// import recipe from "./recipe.js";
+// console.log(recipe);
+
+// // (function displayRecipe() {
+// //   const recipeContainer = document.getElementById("recipe-container");
+
+// //   recipe.forEach((recette) => {
+// //     const recipeElement = document.createElement("div");
+// //     recipeElement.innerHTML = `${recette.name}`;
+// //     recipeContainer.appendChild(recipeElement);
+// //   });
+// // })();
